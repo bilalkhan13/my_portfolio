11d2a4a001fc20a63cbb7dfcb071d9bcccfab55b
bilalkhan13 <bilalahmad.khan@hotmail.com>
2022-09-23 03:20:22 +0500

    Portfolio web app structure is created.

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import Header from './components/header/Header'
+import Nav from './components/nav/Nav'
+import About from './components/about/About'
+import Experience from './components/experience/Experience'
+import Contact from './components/contact/Contact'
+import Portfolio from './components/portfolio/Portfolio'
+import Services from './components/Services/Services'
+import Testimonials from './components/testimonials/Testimonials'
+import Footer from './components/footer/Footer'
+
+const App = () => {
+  return (
+    <>
+      <Header />
+      <Nav />
+      <About />
+      <Experience />
+      <Services />
+      <Portfolio />
+      <Testimonials />
+      <Contact />
+      <Footer />
+    </>
+  )
+}
+
+export default App
